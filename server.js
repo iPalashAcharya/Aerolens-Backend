5021d7e690caffd5b7ec96af1cddb88ec74263ae
iPalashAcharya <palashacharya2004@gmail.com>
2025-08-30 12:05:38 +0530

    made minor changes to deploy backend

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const clientRoutes = require('./routes/client');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/client', clientRoutes);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
