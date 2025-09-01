917fb3b3dc5ce9fbe4235f7f121408e31ec3ea28
iPalashAcharya <palashacharya2004@gmail.com>
2025-09-01 15:19:02 +0530

    added cors configuration

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const clientRoutes = require('./routes/client');
 
 const app = express();
 const PORT = process.env.PORT || 3000;;
 
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+app.set('trust proxy', true);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
