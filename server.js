c5a9fa3f004cb959295bb97fcc41f219e0d6d981
iPalashAcharya <palashacharya2004@gmail.com>
2025-09-17 16:55:27 +0530

    changed set proxy to avoid express seeing all ips as one

--- server.js
+++ server.js
@@ -25,9 +25,10 @@ app.use(cors({
 }));
 app.use(compression());
 
+app.set('trust proxy', 1);
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    max: 500, // limit each IP to 100 requests per windowMs
     message: {
         success: false,
         error: 'RATE_LIMIT_EXCEEDED',
@@ -42,7 +43,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.set('trust proxy', false);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
