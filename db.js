0adda3892266120c7153fe1dfe1191eebd231641
iPalashAcharya <palashacharya2004@gmail.com>
2025-11-09 18:55:54 +0530

    fix the certificate path on the directory

--- db.js
+++ db.js
@@ -13,7 +13,7 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     queueLimit: 0,
     ssl: {
-        ca: fs.readFileSync(path.join(__dirname, 'secret', 'rds_ca.pem'))
+        ca: fs.readFileSync(path.join(__dirname, 'certs', 'rds_ca.pem'))
     }
 });
 
