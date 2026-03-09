d1c400143014d53f2e3752c686dc1b17fa9d8729
iPalashAcharya <palashacharya2004@gmail.com>
2026-03-09 16:06:56 +0530

    get database connection from db instead of creating it

--- scripts/migrateCandidateCompensation.js
+++ scripts/migrateCandidateCompensation.js
@@ -17,12 +17,7 @@ const mysql = require('mysql2/promise');
 
 async function runMigration() {
 
-    const db = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    });
+    const db = require('../db');
 
     console.log("Connected to database");
 
